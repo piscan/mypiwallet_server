82353ddc0e064961f511e167ee17689577509750
johnsavadkuhi <physitime@gmail.com>
2019-12-12 18:01:24 +0330

    add getBalance method

--- src/typedefs/index.js
+++ src/typedefs/index.js
@@ -2,12 +2,31 @@ import { gql } from "apollo-server";
 
 export const  typeDefs = gql`
 
+# type TransactionSent {
+#     blockHash : String 
+#     blockNumber :String
+#     contractAddress:String
+#     cumulativeGasUsed:String
+#     from:String
+#     gasUsed:String
+#     logs:[String]
+#     logsBloom:String
+#     status:Boolean
+#     to:String
+#     transactionHash:String
+#     transactionIndex:Int
+#     nonce:String
+#     gas :String
+# }
+
 type Query {
 
     getBlockNumber:String! 
+    getBalance(address:String):String!
+    # sendRawTransaction(signedTransaction:String):TransactionSent 
 }
+
 type Subscription {
-    blockNumberChanged: String
-  }
+    blockNumberChanged: String  }
 `;  
 
